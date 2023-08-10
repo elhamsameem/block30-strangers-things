309dfa387edadfb9912e2f1939ff02cec2b58a53
phanhkn <phanhkn@gmail.com>
2023-08-10 16:52:51 -0400

    added a login route with button for nav

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router";
 import Register from "./components/Register";
 import "./Style.css";
 import Navbar from "./components/Navbar";
+import Login from "./components/Login";
 
 function App() {
   return (
@@ -11,10 +12,10 @@ function App() {
       <Routes>
         <Route path="/" element={<GetAllPosts />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
-  // <h1> Welcome to Stranger Things! Let's get to work!</h1>;
 }
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,7 @@ function Navbar() {
   return (
     <div className="navbar">
       <Link to={"/register"}>Register</Link>
+      <Link to={"/login"}>Login</Link>
       <Link to={"/"}>Home</Link>
     </div>
   );
