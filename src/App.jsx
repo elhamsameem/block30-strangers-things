026dfa3ee447dc0476235f7d0b4b638c46dcaa0c
phanhkn <phanhkn@gmail.com>
2023-08-10 16:27:45 -0400

    Got started on components, setting up the files to get all posts, a navbar, a register, and one for submitting a post. Still very rudimentary

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import "./App.css";
+import GetAllPosts from "./components/GetAllPosts";
+import { Route, Routes } from "react-router";
+import Register from "./components/Register";
+import "./Style.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  // functionality code here
-
-  return <>{/* your code here */}</>;
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<GetAllPosts />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </div>
+  );
+  // <h1> Welcome to Stranger Things! Let's get to work!</h1>;
 }
 
 export default App;
