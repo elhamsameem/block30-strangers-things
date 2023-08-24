eec516c42929679398dd4af8b9332f7838fe14ec
Elham Sameem <126508470+elhamsameem@users.noreply.github.com>
2023-08-24 19:43:50 -0400

    Added styling and button to add new post

--- src/components/GetAllPosts.jsx
+++ src/components/GetAllPosts.jsx
@@ -1,9 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { getAllPosts } from "../api";
-import PostItem from "./PostItem";
+import PostItem from "./PostItem.jsx";
+import NewPost from "./NewPost";
 
 const GetAllPosts = () => {
   const [posts, setPosts] = useState([]);
+  const [showNewPost, setShowNewPost] = useState(false); // State to control NewPost visibility
+
+  const handleClick = () => {
+    const newPostButtonDiv = document.querySelector(".new-post-button-div");
+    setShowNewPost(true);
+  };
+
   useEffect(() => {
     const fetchPosts = async () => {
       const response = await getAllPosts();
@@ -14,6 +22,15 @@ const GetAllPosts = () => {
   return (
     <div className="posts-container">
       <div className="post">
+        <div className="new-post-button-div">
+          {showNewPost ? (
+            <NewPost /> // Render NewPost component if showNewPost is true
+          ) : (
+            <button className="new-post-button" onClick={handleClick}>
+              Add New Post
+            </button>
+          )}{" "}
+        </div>
         {posts.map((post) => {
           return <PostItem key={post._id} post={post} />;
         })}
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -3,33 +3,35 @@ import "./PostItem.css";
 
 const PostItem = ({ post }) => {
   return (
-    <div className="post-item" key={post.id}>
-      <div className="actions">{post.isAuthor && "Delete"}</div>
-      <div className="post-title">
-        <h2>{post.title}</h2>
-        <b className="seller-username">{post.author.username}</b>
+    <>
+      <div className="post-item" key={post.id}>
+        <div className="actions">{post.isAuthor && "Delete"}</div>
+        <div className="post-title">
+          <h2>{post.title}</h2>
+          <b className="seller-username">{post.author.username}</b>
+        </div>
+        <p>
+          <b>Price: </b>
+          {post.price}
+        </p>
+        <p className="post-description">
+          <b>Description: </b>
+          {post.description}
+        </p>
+        <p>
+          <b>Location: </b>
+          {post.location}
+        </p>
+        <p>
+          <b>Will Deliver: </b> {post.willDeliver ? "Yes" : "No"}
+        </p>
+        <p>
+          <b>Messages: </b>
+          {post.messages}
+        </p>
+        <p>Is Author: {post.isAuthor.toString()}</p>
       </div>
-      <p>
-        <b>Price: </b>
-        {post.price}
-      </p>
-      <p className="post-description">
-        <b>Description: </b>
-        {post.description}
-      </p>
-      <p>
-        <b>Location: </b>
-        {post.location}
-      </p>
-      <p>
-        <b>Will Deliver: </b> {post.willDeliver ? "Yes" : "No"}
-      </p>
-      <p>
-        <b>Messages: </b>
-        {post.messages}
-      </p>
-      <p>Is Author: {console.log(post.isAuthor)}</p>
-    </div>
+    </>
   );
 };
 export default PostItem;
